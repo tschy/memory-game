a5ac2509f31e71d0395e5339e3ec81607464403b
Tamara Schymura <tamara.schymura@studium.fernuni-hagen.de>
2026-01-22 16:35:08 +0100

    fix state

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,24 @@
 import {Card} from "./components/Card";
 import {randomize} from "./util";
+import {useState} from "preact/hooks";
+
+const words = ["flow", "joy", "fun", "thrill", "love", "smile",
+    "peace", "hope", "charm", "glow", "grace", "cheer", "bliss", "pride",
+    "faith", "light", "trust", "zeal"];
+
+const wordOrder = randomize(words.concat(...words));
 
 export function App() {
-    const words = ["flow", "joy", "fun", "thrill", "love", "smile",
-        "peace", "hope", "charm", "glow", "grace", "cheer", "bliss", "pride",
-        "faith", "light", "trust", "zeal"];
-    const wordOrder = randomize(words.concat(...words));
-    const openCards = new Set<number>([0]);
-    const solvedWords = new Set<string>();
-
+    const [openCards, setOpenCards] = useState([] as number[]);
+    const [solvedWords, setSolvedWords] = useState([] as string[]);
     function clickCard(word: string, index: number) {
-        if (openCards.has(index)) return;
+        if (openCards.includes(index)) return;
 
         // if there is another open card with the same word, mark both as solved
 
         // if there are tow other open cards, hide them.
 
-        openCards.add(index);
+        setOpenCards([index, ...openCards]);
     }
 
     return (
@@ -26,8 +28,8 @@ export function App() {
                 <Card
                     word={word}
                     key={word + "-" + i}
-                    solved={solvedWords.has(word)}
-                    open={openCards.has(i)}
+                    solved={solvedWords.includes(word)}
+                    open={openCards.includes(i)}
                     onClick={() => clickCard(word, i)}
                 />
             )}
