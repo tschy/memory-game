a172b76f17e2dbbd76f315d59c0101d3d99979bd
Robert Will <matey.jack@gmail.com>
2026-01-24 21:59:02 +0100

    WIP: Vite PWA as created by amp.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/memory-game/',
-    plugins: [preact()],
+    plugins: [
+        preact(),
+        VitePWA({
+            manifest: {
+                name: 'Memory Game',
+                short_name: 'Memory',
+                description: 'A fun memory card matching game',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                scope: '/memory-game/',
+                start_url: '/memory-game/',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\//,
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'external-assets',
+                            expiration: {
+                                maxEntries: 30,
+                                maxAgeSeconds: 24 * 60 * 60 // 24 hours
+                            }
+                        }
+                    }
+                ]
+            }
+        })
+    ],
 })
